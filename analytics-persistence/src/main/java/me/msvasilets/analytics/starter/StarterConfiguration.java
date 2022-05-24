7f257dec24ff65b31f0fec32283e342b6a61bd0f
Maksim Vasilets <maxvasilets@icloud.com>
2022-05-24 20:16:44 +0300

    build default service bean if not present

--- analytics-persistence/src/main/java/me/msvasilets/analytics/starter/StarterConfiguration.java
+++ analytics-persistence/src/main/java/me/msvasilets/analytics/starter/StarterConfiguration.java
@@ -1,5 +1,10 @@
 package me.msvasilets.analytics.starter;
 
+import me.msvasilets.analytics.starter.domain.AnalyticsRepository;
+import me.msvasilets.analytics.starter.service.AnalyticService;
+import me.msvasilets.analytics.starter.service.SimpleAnalyticService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -9,5 +14,10 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan(basePackages = "me.msvasilets")
 public class StarterConfiguration {
 
+    @Bean
+    @ConditionalOnMissingBean(AnalyticService.class)
+    public SimpleAnalyticService analyticService(AnalyticsRepository repository) {
+        return new SimpleAnalyticService(repository);
+    }
 
 }
